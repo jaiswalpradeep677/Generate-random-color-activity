e744f32ef2cd78263aa059bd887a244aa9209263
Pradeep Kumar Jaiswal <132053047+jaiswalpradeep677@users.noreply.github.com>
2023-08-03 02:49:49 +0530

    Add files via upload

---
+++ activity.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function() {
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+
+     let div = document.querySelector("div");
+     div.style.backgroundColor = randomColor;
+
+    console.log("colour updated");
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let colour = `rgb(${red}, ${green}, ${blue})`;
+    return colour;
+}
